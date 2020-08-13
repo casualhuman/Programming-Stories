e67ab021fba2fa0226d3fea9e6a939f61cbd0881
Rahim Jalloh <abdulrahimjallohaarj@gmail.com>
2020-08-13 04:05:06 +0000

    No commit message

--- randomThought/settings.py
+++ randomThought/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%aq65ps!j51iaxk)yjsnr479-zgp*7#@b!86_$7jp1ie2_5=oo'
+# SECRET_KEY = '%aq65ps!j51iaxk)yjsnr479-zgp*7#@b!86_$7jp1ie2_5=oo'
+SECRET_KEY = os.environ.get('SECRET_KEY_1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
